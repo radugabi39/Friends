3953036ca604f47f74cbc5259799aa6b8e38e576
florinmarius <florinmariusleu@gmail.com>
2016-12-10 13:59:36 +0200

    OrderItem entity for many to many relationship between order and item

--- src/fmi/friends/hibernateEntities/OrderItem.java
+++ src/fmi/friends/hibernateEntities/OrderItem.java
@@ -1,11 +1,91 @@
 package fmi.friends.hibernateEntities;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SecondaryTables;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "ORDER_ITEM", uniqueConstraints = { @UniqueConstraint(columnNames = { "ID" }) })
 public class OrderItem {
+	private int id;
+	
+	private Orders order;
+	
+	private Item item;
+	
+	private Integer quantity;
+	
+	private Date creationDate;
+	
+	public OrderItem() {
+		super();
+	}
+	
+	public OrderItem(int id, Orders order, Item item, Integer quantity, Date creationDate) {
+		super();
+		this.id = id;
+		this.order = order;
+		this.item = item;
+		this.quantity = quantity;
+		this.creationDate = creationDate;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID", nullable = false, unique = true, length = 11)
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ORDER_ID", nullable = false)
+	public Orders getOrder() {
+		return order;
+	}
+	
+	public void setOrder(Orders order) {
+		this.order = order;
+	}
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ITEM_ID", nullable = false)
+	public Item getItem() {
+		return item;
+	}
+	
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	@Column(name = "QUANTITY")
+	public Integer getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Column(name = "CREATIONDATE")
+	public Date getCreationDate() {
+		return creationDate;
+	}
 
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
 }
