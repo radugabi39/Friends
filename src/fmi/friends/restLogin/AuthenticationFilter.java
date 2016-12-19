566cf224d67ac15fcc5581ec9a7a7dcecd0fedb6
razvan.popa <razvan.popa@wecodepixels.com>
2016-12-19 19:41:25 +0200

    Commented filter from AuthenticationFilter.java in order to be able to continue testing.

--- src/fmi/friends/restLogin/AuthenticationFilter.java
+++ src/fmi/friends/restLogin/AuthenticationFilter.java
@@ -12,25 +12,25 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import fmi.friends.restLogin.Secured;
 
-@Provider
-@Priority(Priorities.AUTHENTICATION)
-@Secured
-public class AuthenticationFilter implements ContainerRequestFilter {
-	@Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            throw new NotAuthorizedException("Authorization header must be provided");
-        }
-        
-        String token = authorizationHeader.substring("Bearer".length()).trim();
-
-        try {
-            validateToken(token);
-        } catch (Exception e) {
-            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
-        }
-    }
+//@Provider
+//@Priority(Priorities.AUTHENTICATION)
+//@Secured
+public class AuthenticationFilter {
+//	@Override
+//    public void filter(ContainerRequestContext requestContext) throws IOException {
+//        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+//        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+//            throw new NotAuthorizedException("Authorization header must be provided");
+//        }
+//        
+//        String token = authorizationHeader.substring("Bearer".length()).trim();
+//
+//        try {
+//            validateToken(token);
+//        } catch (Exception e) {
+//            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+//        }
+//    }
 	
 	private void validateToken(String token) {
 		
