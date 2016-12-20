18d692acb4493eb848a19bfecc26cfe9ece96e58
Razvan Popa <razvan.popa@wecodepixels.com>
2016-12-20 13:48:14 +0200

    Finished implementing Posting Reviews to server with parameters.

--- WebContent/WEB-INF/teatrunoras-front/src/js/pages/Show.jsx
+++ WebContent/WEB-INF/teatrunoras-front/src/js/pages/Show.jsx
@@ -13,6 +13,16 @@ import { postReview } from "../actions/reviewsActions.js";
 })
 
 export default class Shows extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   fetchShowAndReviews(id) {
     return dispatch => Promise.all([
       dispatch(fetchSingleShow(id)),
@@ -25,8 +35,17 @@ export default class Shows extends React.Component {
     // this.props.dispatch(postReview());
   }
 
-  postReview(showId, description) {
-    this.props.dispatch(postReview(showId, description)); 
+  // postReview(showId, description) {
+  //   this.props.dispatch(postReview(showId, description)); 
+  // }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.dispatch(postReview(this.props.params['id'], this.state.value));
   }
 
   render() {
@@ -41,7 +60,7 @@ export default class Shows extends React.Component {
     }
 
     return (
-      <div>
+      <div className="single-show-container">
         <h3>{show.name}</h3>
 
         <div>
@@ -54,9 +73,13 @@ export default class Shows extends React.Component {
           <ul>{mappedReviews}</ul>
         </div>
 
-        <textarea className="form-control"></textarea>
-
-        <button className="btn btn-primary" onClick={this.postReview.bind(this, this.props.params['id'], 'This is AMAZING?!!?!?')}>Post Review</button>
+        <form onSubmit={this.handleSubmit} className="add-review-form">
+          <label>
+            <p>Add Review:</p>
+            <textarea className="form-control" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input className="btn btn-primary" type="submit" value="Submit Review" />
+        </form>
       </div>
     );
   }
