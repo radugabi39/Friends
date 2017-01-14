2c99223ff2aee4fd44d2093659d94b62439f4a84
radugabi39 <radu.gabi93@yahoo.com>
2017-01-14 17:13:36 +0200

    g

--- src/fmi/friends/restLogin/AuthenticationFilter.java
+++ src/fmi/friends/restLogin/AuthenticationFilter.java
@@ -10,29 +10,42 @@ import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
+
+import fmi.friends.dao.UserDAO;
 import fmi.friends.restLogin.Secured;
 
-//@Provider
-//@Priority(Priorities.AUTHENTICATION)
-//@Secured
-public class AuthenticationFilter {
-//	@Override
-//    public void filter(ContainerRequestContext requestContext) throws IOException {
-//        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
-//        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-//            throw new NotAuthorizedException("Authorization header must be provided");
-//        }
-//        
-//        String token = authorizationHeader.substring("Bearer".length()).trim();
-//
-//        try {
-//            validateToken(token);
-//        } catch (Exception e) {
-//            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
-//        }
-//    }
-	
-	private void validateToken(String token) {
-		
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+// @Secured
+public class AuthenticationFilter implements ContainerRequestFilter {
+	UserDAO userDAO = new UserDAO();
+	final String urlLogin = "authentication/login";
+
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		// if(requestContext.get)
+		if (requestContext.getUriInfo().getRequestUri() != null
+				&& requestContext.getUriInfo().getRequestUri().getPath().indexOf(urlLogin) == -1) {
+			String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+			if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+				throw new NotAuthorizedException("Authorization header must be provided");
+			}
+
+			String token = authorizationHeader.substring("Bearer".length()).trim();
+
+			try {
+				validateToken(token);
+			} catch (Exception e) {
+				requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+			}
+		}
+
+	}
+
+	private void validateToken(String token) throws Exception {
+		boolean response = userDAO.checkToken(token);
+		if (response == false) {
+			throw new Exception();
+		}
+
 	}
 }
