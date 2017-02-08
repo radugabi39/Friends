b3aaaca91e734f7149b9a744f84a9775dca3bc85
radugabi39 <radu.gabi93@yahoo.com>
2017-02-08 15:29:38 +0200

    update pentru nr voturi si rating poer review

--- src/fmi/friends/models/ReviewModelUPModel.java
+++ src/fmi/friends/models/ReviewModelUPModel.java
@@ -7,12 +7,16 @@ public class ReviewModelUPModel {
 	
 	private String descrReview;
 	private Integer points;
+	private BigDecimal rating;
+	private Integer noVotes;
 	
 	public ReviewModelUPModel(String descrReview, BigDecimal rating,Integer noVotes) {
 		super();
 		this.descrReview = descrReview;
 		if(noVotes!=null && rating!=null)
 		this.points = rating.multiply(new BigDecimal(noVotes)).intValue();
+		this.rating = rating;
+		this.noVotes=noVotes;
 	}
 	
 	public String getDescrReview() {
