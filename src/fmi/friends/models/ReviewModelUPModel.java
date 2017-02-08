63b9b288ea43b3aa71a413e2beaf60e2537f5505
radugabi39 <radu.gabi93@yahoo.com>
2017-02-08 16:15:43 +0200

    sa

--- src/fmi/friends/models/ReviewModelUPModel.java
+++ src/fmi/friends/models/ReviewModelUPModel.java
@@ -15,7 +15,7 @@ public class ReviewModelUPModel {
 		this.descrReview = descrReview;
 		if(noVotes!=null && rating!=null)
 		this.points = rating.multiply(new BigDecimal(noVotes)).intValue();
-		this.rating = rating;
+		this.rating=rating;
 		this.noVotes=noVotes;
 	}
 	
@@ -30,6 +30,22 @@ public class ReviewModelUPModel {
 	}
 	public void setPoints(Integer points) {
 		this.points = points;
+	}
+
+	public Integer getNoVotes() {
+		return noVotes;
+	}
+
+	public void setNoVotes(Integer noVotes) {
+		this.noVotes = noVotes;
+	}
+
+	public BigDecimal getRating() {
+		return rating;
+	}
+
+	public void setRating(BigDecimal rating) {
+		this.rating = rating;
 	}
 	
 }
