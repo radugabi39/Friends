866717f755957580f06948442769f572776c0da0
razvan.popa <razvan.popa@wecodepixels.com>
2017-02-08 14:27:27 +0200

    Implemented styling for Homepage.

--- WebContent/WEB-INF/teatrunoras-front/src/js/components/ShowSlider.jsx
+++ WebContent/WEB-INF/teatrunoras-front/src/js/components/ShowSlider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ImageGallery from 'react-image-gallery';
 import { connect } from "react-redux";
 import { fetchShowsByReviews } from "../actions/showsActions";
+import { IndexLink, Link } from "react-router";
 
 @connect((store) => {
   return {
@@ -26,12 +27,19 @@ export default class ShowSlider extends React.Component {
       mappedShowsByReviews = [];
     }
     else {
-      console.log(showsByReviews);
       for (var i=0; i < 4; i++) {
+        let linkText = (
+          <div className="sliderLinkText">
+            <div className="sliderLinkTextDesc">{showsByReviews.list[i].description}</div>
+            <div className="sliderLinkTextName"><span>{showsByReviews.list[i].name}</span></div>
+          </div>
+        );
+        let link = <Link to={"show/" + showsByReviews.list[i].id} className="sliderLink">{linkText}</Link>
         mappedShowsByReviews.push({
           original: showsByReviews.list[i].avatarURL,
           thumbnail: showsByReviews.list[i].avatarURL,
-          description: showsByReviews.list[i].name + '. ' + showsByReviews.list[i].description,
+          description: link
+          // description: showsByReviews.list[i].name + '. ' + showsByReviews.list[i].description,
         });
       }
     }
--- WebContent/WEB-INF/teatrunoras-front/src/js/pages/Home.js
+++ WebContent/WEB-INF/teatrunoras-front/src/js/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchShowsByCreationDate } from "../actions/showsActions";
-
+import { IndexLink, Link } from "react-router";
 import ShowSlider from "../components/ShowSlider.jsx";
 
 @connect((store) => {
@@ -21,7 +21,19 @@ export default class Home extends React.Component {
   	var mappedShowsByDate = [];
     if (typeof showsByDate.list !== 'undefined') {
       for (var i=0; i < 6; i++) {
-      	mappedShowsByDate.push(<li>{showsByDate.list[i].name}</li>)
+      	mappedShowsByDate.push(
+          <li className="col-md-4 homeRecentShowsLi">
+            <div style={{backgroundImage: 'url(' + showsByDate.list[i].avatarURL + ')'}} className="homeRecentShowsLiContainer">
+              <div className="homeRecentShowsLink">
+                <div className="grayOverlay">
+                  <Link to={"show/" + showsByDate.list[i].id}>
+                    {showsByDate.list[i].name}
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </li>
+        )
       }
     }
 
@@ -32,8 +44,10 @@ export default class Home extends React.Component {
 	    	<h2>Cele mai discutate spectacole</h2>
   			<ShowSlider />  	
 
-  			<h2>Cele mai recente spectacole</h2>
-	    	<ul>{mappedShowsByDate}</ul>
+        <div className="container">
+    			<h2>Cele mai recente spectacole</h2>
+  	    	<ul className="row homeRecentShowsUl">{mappedShowsByDate}</ul>
+        </div>
     	</div>
     );
   }
